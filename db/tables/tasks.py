159e8fa2c18dd0044a10603a3ace85528b65b270
WirelessWisdom <ninakazimirskaya@gmail.com>
2024-03-14 20:42:09 +0100

    added UserRole class to user.py added UUID, role column, changed some columns nullable=True in users.py added column assignee, UUID, changed some columns nullable=True, added ForeignKey to columns 'created_by' and 'assignee'

--- db/tables/tasks.py
+++ db/tables/tasks.py
@@ -1,16 +1,19 @@
-from sqlalchemy import Column, PrimaryKeyConstraint, String, Table, UUID, Enum, DateTime
+from sqlalchemy import Column, PrimaryKeyConstraint, String, Table, UUID, Enum, DateTime, ForeignKey
+import sqlalchemy
 from models import TaskStatus
 from base import metadata
 
 tasks = Table(
     'tasks',
     metadata,
-    Column('id', UUID, nullable=False),
+    Column('id', UUID, nullable=False, server_default=sqlalchemy.text('gen_random_uuid()')),
     Column('name', String, nullable=False),
-    Column('description', String, nullable=False),
+    Column('description', String, nullable=True),
     Column('status', Enum(TaskStatus), nullable=False),
     Column('created_at', DateTime, nullable=False),
-    Column('due_date', DateTime, nullable=False),
-    Column('user', String, nullable=False),
+    Column('due_date', DateTime, nullable=True),
+    Column('created_by', String, ForeignKey('users.id'), nullable=False),
+    Column('assignee', String, ForeignKey('users.id'), nullable=True),
     PrimaryKeyConstraint('id', name='task_pk'),
+
 )
