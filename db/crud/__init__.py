6c0a26b643a01a678a9a3eac93251ac03f173ec4
WirelessWisdom <ninakazimirskaya@gmail.com>
2024-03-19 21:15:52 +0100

    added base_crud.py (currently refactored create, get and delete functions)
    modified (added creation of objects):
    crud_tasks.py
    crud_users.py
    __init__.py

---
+++ db/crud/__init__.py
@@ -0,0 +1,7 @@
+from .crud_tasks import tasks_crud
+from .crud_users import users_crud
+
+__all__ = [
+    'tasks_crud',
+    'users_crud'
+]
---
+++ db/crud/crud_users.py
@@ -0,0 +1,5 @@
+from db.crud.base_crud import BaseCrud
+from db.tables import users
+
+
+users_crud = BaseCrud(users)
